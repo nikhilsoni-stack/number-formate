3afa27ee96792568af314afeab2d8b2ed1fa28e4
Nikhil <aanikhilsoni@gmail.com>
2024-03-25 16:52:21 +0530

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,7 @@
 import { Input } from "./types";
 export declare function addingCommasToNumber(input: Input): string;
+export declare function convertToFixed(num: string, toFixed: number): string | number;
+export declare function getDecimal(el: string, prevValue: string): string;
+export declare function get7DecimalAtMax(el: string, prevValue: string): string;
+export declare const convertToPaisa: (value: string) => number;
+export declare const convertToRupees: (value: string) => number;
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.addingCommasToNumber = void 0;
+exports.convertToRupees = exports.convertToPaisa = exports.get7DecimalAtMax = exports.getDecimal = exports.convertToFixed = exports.addingCommasToNumber = void 0;
+// adding commas to number
 function addingCommasToNumber(input) {
     let { x, onlyNum } = input;
     if (!x) {
@@ -35,3 +36,46 @@ function addingCommasToNumber(input) {
     }
 }
 exports.addingCommasToNumber = addingCommasToNumber;
+function convertToFixed(num, toFixed) {
+    if (!num) {
+        return "0";
+    }
+    let x = Number(num);
+    if (toFixed != null || toFixed != undefined) {
+        return x.toFixed(toFixed);
+    }
+    return x;
+}
+exports.convertToFixed = convertToFixed;
+function getDecimal(el, prevValue) {
+    var ex = /^\d*\.?\d{0,2}$/;
+    if (ex.test(el) == false) {
+        el = prevValue != null ? prevValue : el.substring(0, el.length - 1);
+    }
+    return el;
+}
+exports.getDecimal = getDecimal;
+function get7DecimalAtMax(el, prevValue) {
+    var ex = /^\d*\.?\d{0,7}$/;
+    if (ex.test(el) == false) {
+        el = prevValue != null ? prevValue : el.substring(0, el.length - 1);
+    }
+    return el;
+}
+exports.get7DecimalAtMax = get7DecimalAtMax;
+const convertToPaisa = (value) => {
+    if (value) {
+        let newValue = parseFloat(value) * 100;
+        return isNaN(newValue) ? 0 : Math.round(newValue);
+    }
+    return 0;
+};
+exports.convertToPaisa = convertToPaisa;
+const convertToRupees = (value) => {
+    if (value) {
+        let newValue = parseFloat(value) / 100;
+        return newValue;
+    }
+    return 0;
+};
+exports.convertToRupees = convertToRupees;
